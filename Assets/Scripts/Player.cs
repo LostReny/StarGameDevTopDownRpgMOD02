21cebe072a05652c1b0d8808c9974cb9650d238e
Isabelle Reny <isabelleribaldo@hotmail.com>
2023-05-11 01:49:45 -0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //variavel -memória do pc - armazena diversos dados 
+    public int vidaPlayer = 10; //n inteiro
+    public float speedPlayer = 5f; // valor flutuante 
+    public string meuPlayer = "Gandalf"; // texto 
+    public bool estaVivo; // verdadeiro ou falso
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
