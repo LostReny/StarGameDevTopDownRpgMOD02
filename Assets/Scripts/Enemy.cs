8138b556759d57725a646a79e955e58e84266c98
Isabelle Reny <isabelleribaldo@hotmail.com>
2023-05-13 21:24:40 -0300

    Classes, metodos e variáveis

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int healthEnemy = 10;
+    public float speedEnemy = 5f;
+
+    public void Moving(){
+
+    }
+
+    public void SeekPlayer(){
+
+    }
+
+    public void ChasePlayer(){
+
+    }
+
+    public void Attack(){
+        
+    }
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,26 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// CLASSE AGRUPA VARIAVEIS E MÉTODOS
 public class Player : MonoBehaviour
 {
 
     //variavel -memória do pc - armazena diversos dados 
-    public int vidaPlayer = 10; //n inteiro
-    public float speedPlayer = 5f; // valor flutuante 
-    public string meuPlayer = "Gandalf"; // texto 
-    public bool estaVivo; // verdadeiro ou falso
+    public int health = 10; //n inteiro
+    public float speed = 5f; // valor flutuante 
 
+    //Metodos - instruções 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Attack(){
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Move(){
+
+    }
+
+    void Jump(){
+
     }
 
 }
